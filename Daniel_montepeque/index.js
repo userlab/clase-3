e04b8516c102151629c6406f08aad4568afe4630
eldaniell01 <amontepequem@miumg.edu.gt>
2022-04-08 18:43:23 -0600

    add functions javascrips

---
+++ Daniel_montepeque/index.js
@@ -0,0 +1,16 @@
+function searchg(){
+    var search = document.getElementById("searchg");
+    window.location.href='https://www.google.com/search?q='+search.value;
+}
+
+const luck = () =>{
+    window.location.href='https://www.google.com/doodles';
+}
+
+const products = ()=>{
+    window.location.href='https://www.google.com.gt/intl/es-419/about/products'; 
+}
+
+const login =()=>{
+    window.location.href='https://accounts.google.com/ServiceLogin?hl=es-419&amp;passive=true&amp;continue=https://www.google.com/&amp;ec=GAZAmgQ';
+}
